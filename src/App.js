b8f4db37de9bb0ebdc0ae13e06ec6c7a1cd4df23
Vladislav <vladislavxperiaz3@gmail.com>
2021-12-15 21:14:03 +0200

    18. gh-pages

--- src/App.js
+++ src/App.js
@@ -4,43 +4,10 @@ import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import {connect} from "react-redux";
 import MainWindow from "./components/MainWindow/MainWindow";
 
-const getObjectProperty = (obj, path, defaultValue = undefined) => {
-    const arrPath = path.toString().split('.')
-    if ((arrPath.length === 0) || (obj[arrPath[0]] === undefined)) {
-        console.log(defaultValue)
-        return
-    }
-    let resultValue = obj[arrPath[0]];
-    for (let i = 1; i < arrPath.length; i++) {
-        resultValue = resultValue[arrPath[i]]
-        if (resultValue === undefined) {
-            console.log(defaultValue)
-            return
-        }
-    }
-    console.log(resultValue)
-}
-
-const obj = {
-    'pupa': {
-        'lupa': {
-            'beep': 'boop',
-        },
-        'foo': 'bar',
-    },
-}
-getObjectProperty(obj, "pupa.lupa"); // > { beep : 'boop' }
-getObjectProperty(obj, "pupa.lupa.beep"); // > 'boop'
-getObjectProperty(obj, "pupa.foo"); // > 'bar'
-getObjectProperty(obj, "pupa.ne.tuda"); // > undefined
-getObjectProperty(obj, "pupa.ne.tuda", true); // > true
-getObjectProperty(obj, "pupa.ne.tuda", "Default value"); // > 'Default value'
-getObjectProperty(obj, 123, "Default value"); // > 'Default value'
-
 const App = (props) => {
     return (
       <div className="App">
-          <BrowserRouter>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
               <Switch>
                   <Route exact path='/' render={() =>
                     props.isConfirm ? <Redirect to={'/app'}/> : <Redirect to={'/login'}/>}/>
